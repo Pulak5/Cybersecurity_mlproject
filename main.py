f068ae2f45647e58bde8b868143c5c8341c44817
Pulak5 <pulak@example.com>
2026-06-02 15:12:20 +0530

    adding data transformation

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import sys
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
-from networksecurity.components.data_ingestion import DataIngestion,DataIngestionArtifact
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig,TrainingPipelineConfig
+from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
-from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.logging.logger import logging
 
 if __name__=="__main__":
@@ -12,14 +12,20 @@ if __name__=="__main__":
         dataingconfig=DataIngestionConfig(training_pipeline_config=train_pipe)
         obj=DataIngestion(data_ingestion_config=dataingconfig)
         logging.info("Initaiting data ingestion")
-        dataingestionconfig=obj.initiate_data_ingestion()
+        dataingestionartifact=obj.initiate_data_ingestion()
         logging.info("Data initiation completed")
-        print(dataingestionconfig)
-        data_validation=DataValidation(data_ingestion_artifact=DataIngestionArtifact(trained_file_path=dataingestionconfig.trained_file_path,test_file_path=dataingestionconfig.test_file_path),data_validation_config=DataValidationConfig(training_pipeline_config=train_pipe))
+        print(dataingestionartifact)
+        data_validation=DataValidation(data_ingestion_artifact=dataingestionartifact,data_validation_config=DataValidationConfig(training_pipeline_config=train_pipe))
         logging.info("Initiate data validation")
         data_validation_artifact=data_validation.initiate_data_validation()
         logging.info("Data validation completed")
         print(data_validation_artifact)
+        data_transformation_config=DataTransformationConfig(training_pipeline_config=train_pipe)
+        data_transformation=DataTransformation(data_transformation_config,data_validation_artifact)
+        logging.info("Initiating data transformation")
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        logging.info("Data transformation completed")
+        print(data_transformation_artifact)
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -3,7 +3,6 @@ from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 import os,sys
 import numpy as np
-#import dill
 import pickle
 
 def read_yaml_file(file_path: str)->dict:
@@ -22,5 +21,24 @@ def write_yaml_file(file_path: str,content: object,replace: bool=False)->None:
             os.makedirs(os.path.dirname(file_path),exist_ok=True)
             with open(file_path,"w") as file:
                 yaml.dump(content,file)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+
+def save_numpy_array_data(file_path: str,array: np.array):
+    try:
+        dir_path=os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path,"wb") as file:
+            np.save(file,array)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+
+def save_object(file_path: str,obj: object)->None:
+    try:
+        logging.info("Entered the save object method")
+        os.makedirs(os.path.dirname(file_path),exist_ok=True)
+        with open(file_path,"wb") as file:
+            pickle.dump(obj=object,file=file)
+        logging.info("Exited the save object method and saved the object")
     except Exception as e:
         raise NetworkSecurityException(e,sys)
